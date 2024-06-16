2f3a415a4dc09eaae2b937a6bdff9669e46b8d2b
jiwon <kiwijomn@gmail.com>
2024-06-17 01:55:35 +0900

    OFZ-4 init: project 세팅

---
+++ src/main/java/com/worknector/offizz/OffizzApplication.java
@@ -0,0 +1,13 @@
+package com.worknector.offizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffizzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OffizzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/worknector/offizz/OffizzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worknector.offizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffizzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
