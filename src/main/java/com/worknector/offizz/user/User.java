28fabd641c2b10b54150cc56458caccfd21f2235
jiwon <kiwijomn@gmail.com>
2024-06-17 02:29:38 +0900

    OFZ-4 feat: User Entity 기본 필드 추가

---
+++ src/main/java/com/worknector/offizz/user/User.java
@@ -0,0 +1,25 @@
+package com.worknector.offizz.user;
+
+import com.worknector.offizz.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private Long socialId;
+
+    private String nickName;
+}
