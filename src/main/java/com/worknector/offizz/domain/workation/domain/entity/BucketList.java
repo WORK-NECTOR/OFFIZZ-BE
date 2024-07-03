592c8eb87f44a3f8cc56191207e97d63e7ef918e
jiwon <kiwijomn@gmail.com>
2024-07-03 21:44:57 +0900

    feat: BucketList isComplete 컬럼 추가

--- src/main/java/com/worknector/offizz/domain/workation/domain/entity/BucketList.java
+++ src/main/java/com/worknector/offizz/domain/workation/domain/entity/BucketList.java
@@ -23,4 +23,8 @@ public class BucketList extends BaseEntity {
     private Workation workation;
 
     private String name;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private boolean isComplete = false;
 }
