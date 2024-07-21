b877ba1656ae3947ddd53808b3c59a068ad5c40d
jiwon <g10823@hamagroups.io>
2024-07-21 11:22:21 +0900

    feat: 숙박시설 Mapper 클래스 추가

---
+++ src/main/java/com/worknector/offizz/openapi/tour/application/mapper/AccommodationMapper.java
@@ -0,0 +1,39 @@
+package com.worknector.offizz.openapi.tour.application.mapper;
+
+import com.worknector.offizz.openapi.tour.application.dto.AccommodationResponse;
+import com.worknector.offizz.openapi.tour.domain.Accommodation;
+
+public class AccommodationMapper {
+
+    private AccommodationMapper() {
+        throw new IllegalArgumentException();
+    }
+
+    public static Accommodation toEntity(AccommodationResponse.Item item) {
+        return Accommodation.builder()
+                .addr1(item.getAddr1())
+                .cpyrhtDivCd(item.getCpyrhtDivCd())
+                .mapy(item.getMapy())
+                .mlevel(item.getMlevel())
+                .modifiedtime(item.getModifiedtime())
+                .sigungucode(item.getSigungucode())
+                .tel(item.getTel())
+                .title(item.getTitle())
+                .contentid(item.getContentid())
+                .contenttypeid(item.getContenttypeid())
+                .createdtime(item.getCreatedtime())
+                .benikia(item.getBenikia())
+                .goodstay(item.getGoodstay())
+                .hanok(item.getHanok())
+                .firstimage(item.getFirstimage())
+                .firstimage2(item.getFirstimage2())
+                .mapx(item.getMapx())
+                .addr2(item.getAddr2())
+                .areacode(item.getAreacode())
+                .booktour(item.getBooktour())
+                .cat1(item.getCat1())
+                .cat2(item.getCat2())
+                .cat3(item.getCat3())
+                .build();
+    }
+}
