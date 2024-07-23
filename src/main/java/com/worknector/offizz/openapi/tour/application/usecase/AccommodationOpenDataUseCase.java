d604ad93d24ed881a9747f802dfb8190d561af42
jiwon <kiwijomn@gmail.com>
2024-07-24 01:50:31 +0900

    refactor: AccommodationOpenDataUseCase else 구문 제거

--- src/main/java/com/worknector/offizz/openapi/tour/application/usecase/AccommodationOpenDataUseCase.java
+++ src/main/java/com/worknector/offizz/openapi/tour/application/usecase/AccommodationOpenDataUseCase.java
@@ -32,31 +32,30 @@ public class AccommodationOpenDataUseCase {
             int currentRows = accommodationResponse.response().body().numOfRows();
             int totalCount = accommodationResponse.response().body().totalCount();
 
-            if (currentRows != 0 && items != null) {
-                List<AccommodationResponse.Item> accommodationItemList = items.item();
-                if (accommodationItemList.isEmpty()) {
-                    break;
+            if (items == null || currentRows == 0) {
+                break;
+            }
+
+            List<AccommodationResponse.Item> accommodationItemList = items.item();
+            if (accommodationItemList.isEmpty()) {
+                break;
+            }
+
+            for (AccommodationResponse.Item item : accommodationItemList) {
+                Optional<Accommodation> existingAccommodation = accommodationRepository.findByContentid(item.contentid());
+
+                if (existingAccommodation.isPresent()) {
+                    // TODO: 기존 데이터 업데이트 로직 추가
+                    continue;
                 }
 
-                for (AccommodationResponse.Item item : accommodationItemList) {
-                    Optional<Accommodation> existingAccommodation = accommodationRepository.findByContentid(item.contentid());
+                // 새 데이터 삽입
+                Accommodation accommodationEntity = AccommodationMapper.toEntity(item);
+                accommodationEntityList.add(accommodationEntity);
+            }
 
-                    if (existingAccommodation.isPresent()) {
-//                      Accommodation existingEntity = existingAccommodation.get();
-//                      Accommodation updatedEntity = AccommodationMapper.toEntity(item);
-                        // 기존 데이터 업데이트
-                    } else {
-                        // 새 데이터 삽입
-                        Accommodation accommodationEntity = AccommodationMapper.toEntity(item);
-                        accommodationEntityList.add(accommodationEntity);
-                    }
-                }
-
-                int lastPage = (int) Math.ceil((double) totalCount / numOfRows);
-                if (pageNo >= lastPage) {
-                    break;
-                }
-            } else {
+            int lastPage = (int) Math.ceil((double) totalCount / numOfRows);
+            if (pageNo >= lastPage) {
                 break;
             }
 
