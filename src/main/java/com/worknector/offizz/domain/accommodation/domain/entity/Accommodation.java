19733ed55be334a2e849903d9aef02e14f0f25a7
yang <ywj9811@gmail.com>
2024-07-25 05:52:08 +0900

    OFZ-15 fix : 불필요한 컬럼 제거

---
+++ src/main/java/com/worknector/offizz/domain/accommodation/domain/entity/Accommodation.java
@@ -0,0 +1,44 @@
+package com.worknector.offizz.domain.accommodation.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Accommodation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String addr1;
+
+    private String addr2; // 와이어프레임상 사용하지 않는 것으로 보이는데, 불필요시 삭제
+
+    private String mapx; // map 관련 또한 사용하지 않는 것으로 보이는데, 불필요시 삭제
+
+    private String mapy;
+
+    private String mlevel;
+
+    private String modifiedtime; // 업데이트시 사용?
+
+    private String tel;
+
+    private String title;
+
+    private String contentid;
+
+    private String firstimage;
+
+    private String firstimage2;
+}
