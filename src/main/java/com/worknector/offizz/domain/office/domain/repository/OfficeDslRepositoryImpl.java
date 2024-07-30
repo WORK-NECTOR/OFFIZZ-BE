b9a4f30304ab5d4d70627438848087785ebec64e
yang <ywj9811@gmail.com>
2024-07-31 05:15:17 +0900

    OFZ-15 fix : 지역 요청시 '기타'에 따른 동적 쿼리 사용하도록 수정

--- src/main/java/com/worknector/offizz/domain/office/domain/repository/OfficeDslRepositoryImpl.java
+++ src/main/java/com/worknector/offizz/domain/office/domain/repository/OfficeDslRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.worknector.offizz.domain.office.domain.repository;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.worknector.offizz.domain.office.application.dto.req.Region;
 import com.worknector.offizz.domain.office.domain.entity.Office;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -18,27 +20,39 @@ public class OfficeDslRepositoryImpl implements OfficeDslRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public List<Office> findRecommendByRegion(String region) {
+    public List<Office> findRecommendByRegion(Region region) {
         return queryFactory.selectFrom(office)
-                .where(office.streetAddress.contains(region))
+                .where(regionBuilder(region))
                 .orderBy(office.hit.desc())
                 .fetch();
     }
 
     @Override
-    public Page<Office> findAllPagingByRegion(String region, Pageable pageable) {
+    public Page<Office> findAllPagingByRegion(Region region, Pageable pageable) {
         List<Office> offices = queryFactory.selectFrom(office)
-                .where(office.streetAddress.contains(region))
+                .where(regionBuilder(region))
                 .orderBy(office.hit.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        Long total = queryFactory.select(office.count())
+        long total = queryFactory.select(office.count())
                 .from(office)
-                .where(office.streetAddress.contains(region))
+                .where(regionBuilder(region))
                 .fetchOne();
 
         return new PageImpl<>(offices, pageable, total);
+    }
+
+    private BooleanBuilder regionBuilder(Region region) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (region.isEtc()) {
+            for (Region nonEtcRegion : Region.getNonEtcRegions()) {
+                builder.and(office.streetAddress.contains(nonEtcRegion.toString()).not());
+            }
+            return builder;
+        }
+
+        return builder.and(office.streetAddress.contains(region.toString()));
     }
 }
