4c2629a850aab1880eafe34d14349969208a7547
jiwon <g10823@hamagroups.io>
2024-08-05 18:25:37 +0900

    feat: 통합 테스트 custom annotation 추가

---
+++ src/test/java/com/worknector/offizz/utils/annotation/CustomIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.worknector.offizz.utils.annotation;
+
+import com.worknector.offizz.global.config.swagger.SwaggerConfig;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ActiveProfiles("test")
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@TestPropertySource("classpath:application-test.yml")
+@ComponentScan(
+        basePackages = {
+                "com.worknector.offizz.global.config"
+        },
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {SwaggerConfig.class,})
+)
+public @interface CustomIntegrationTest {
+
+}
