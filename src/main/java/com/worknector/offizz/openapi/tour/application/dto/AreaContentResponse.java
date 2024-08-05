29a66b04a53de1f4d4100da1528abfe94a788660
jiwon <kiwijomn@gmail.com>
2024-08-06 02:24:28 +0900

    feat: 지역 기반 관광정보 Response 추가

---
+++ src/main/java/com/worknector/offizz/openapi/tour/application/dto/AreaContentResponse.java
@@ -0,0 +1,49 @@
+package com.worknector.offizz.openapi.tour.application.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.List;
+
+public record AreaContentResponse(Response response) {
+
+    public record Response(Header header, Body body) {
+    }
+
+    public record Header(String resultCode, String resultMsg) {
+    }
+
+    public record Body(
+            @JsonInclude(JsonInclude.Include.NON_NULL)
+            Items items,
+            int numOfRows,
+            int pageNo,
+            int totalCount) {
+    }
+
+    public record Items(List<Item> item) {
+    }
+
+    public record Item(
+            String firstimage,
+            String firstimage2,
+            String mapx,
+            String mapy,
+            String mlevel,
+            String addr2,
+            String areacode,
+            String modifiedtime,
+            String cpyrhtDivCd,
+            String booktour,
+            String cat1,
+            String sigungucode,
+            String tel,
+            String title,
+            String addr1,
+            String cat2,
+            String cat3,
+            String contentid,
+            String contenttypeid,
+            String createdtime,
+            String zipcode) {
+    }
+}
