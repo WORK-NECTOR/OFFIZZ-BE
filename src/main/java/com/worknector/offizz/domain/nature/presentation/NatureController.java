e1b564698927a41822ae4c58be5e8ae61584b14b
jiwon <kiwijomn@gmail.com>
2024-08-16 23:28:12 +0900

    refactor: 페이지네이션 request param 통일

--- src/main/java/com/worknector/offizz/domain/nature/presentation/NatureController.java
+++ src/main/java/com/worknector/offizz/domain/nature/presentation/NatureController.java
@@ -6,10 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,11 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class NatureController {
     private final RecommendNatureUseCase recommendNatureUseCase;
 
-    @GetMapping("/recommend")
+    @GetMapping("/recommend/{page}/{size}")
     @Operation(summary = "VACATION > 큐레이션) 검색에 따른 자연 페이지", description = "'서울 강남' 검색 -> 서울과 강남을 모두 포함하는 주소 혹은 자연 관광지 이름")
     public ResponseEntity<RecommendNaturePagingResponse> searchRecommendNature(@RequestParam(name = "search") String search,
-                                                                               @RequestParam(name = "page") int page,
-                                                                               @RequestParam(name = "size") int size)
+                                                                               @PathVariable(name = "page") int page,
+                                                                               @PathVariable(name = "size") int size)
     {
         RecommendNaturePagingResponse allSearchNature = recommendNatureUseCase.searchRecommendNature(search, page, size);
         return ResponseEntity.ok(allSearchNature);
