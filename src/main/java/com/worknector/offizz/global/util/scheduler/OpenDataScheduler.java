30edb83a2ec48524a2354ae6cd09a6be2c06004a
kiwijomn <kiwijomn@gmail.com>
2024-08-20 21:16:40 +0900

    feat: 음식점 데이터 적재 scheduler 로직 추가

--- src/main/java/com/worknector/offizz/global/util/scheduler/OpenDataScheduler.java
+++ src/main/java/com/worknector/offizz/global/util/scheduler/OpenDataScheduler.java
@@ -3,17 +3,23 @@ package com.worknector.offizz.global.util.scheduler;
 import com.worknector.offizz.openapi.course.application.usecase.CourseOpenDataUseCase;
 import com.worknector.offizz.openapi.office.application.usecase.OfficeOpenDataUseCase;
 import com.worknector.offizz.openapi.tour.application.usecase.AccommodationOpenDataUseCase;
+import com.worknector.offizz.openapi.tour.application.usecase.RestaurantOpenDataUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class OpenDataScheduler {
 
+    private static final List<String> RESTAURANT_CAT3_LIST = List.of("A05020100", "A05020200", "A05020300", "A05020400", "A05020700");
+
     private final CourseOpenDataUseCase courseOpenDataUseCase;
     private final OfficeOpenDataUseCase officeDataUseCase;
     private final AccommodationOpenDataUseCase accommodationOpenDataUseCase;
+    private final RestaurantOpenDataUseCase restaurantOpenDataUseCase;
 
     // 공유오피스 데이터 매달 1일 자정마다 update
     @Scheduled(cron = "0 0 0 1 * ?")
@@ -31,5 +37,11 @@ public class OpenDataScheduler {
     @Scheduled(cron = "0 0 0 1 * ?")
     public void scheduleAccommodationOpenDataUpdate() {
         accommodationOpenDataUseCase.updateAccommodationData();
+    }
+
+    // 음식점 데이터 매달 1일 자정마다 update
+    @Scheduled(cron = "0 0 0 1 * ?")
+    public void scheduleRestaurantOpenDataUpdate() {
+        restaurantOpenDataUseCase.updateRestaurantData(RESTAURANT_CAT3_LIST);
     }
 }
