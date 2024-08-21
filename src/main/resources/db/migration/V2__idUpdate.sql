50e52bb8565665f14dfa95516bb358cdbfc2ed38
yang <ywj9811@gmail.com>
2024-08-22 05:33:31 +0900

    OFZ-28 feat : 변경점 쿼리 추가

--- src/main/resources/db/migration/V2__idUpdate.sql
+++
@@ -1,8 +0,0 @@
--- accommodation 테이블 id 컬럼명 변경
-ALTER TABLE accommodation CHANGE id accommodation_id BIGINT;
-
--- cafe 테이블 id 컬럼명 변경
-ALTER TABLE cafe CHANGE id cafe_id BIGINT;
-
--- office 테이블 id 컬럼명 변경
-ALTER TABLE office CHANGE id office_id BIGINT;
