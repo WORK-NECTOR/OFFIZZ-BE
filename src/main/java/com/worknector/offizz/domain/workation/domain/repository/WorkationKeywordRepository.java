1d85596732672293a8e5379a4e7c7bcc27462fe5
jiwon <kiwijomn@gmail.com>
2024-08-28 23:13:09 +0900

    feat: WorkationKeyword repository 레이어 추가

---
+++ src/main/java/com/worknector/offizz/domain/workation/domain/repository/WorkationKeywordRepository.java
@@ -0,0 +1,7 @@
+package com.worknector.offizz.domain.workation.domain.repository;
+
+import com.worknector.offizz.domain.workation.domain.entity.WorkationKeyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkationKeywordRepository extends JpaRepository<WorkationKeyword, Long> {
+}
