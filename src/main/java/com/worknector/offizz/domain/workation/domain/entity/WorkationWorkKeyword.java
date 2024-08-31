f8bca53e46da9bdd7db0505250e3fa3da64fb982
jiwon <kiwijomn@gmail.com>
2024-08-31 15:57:03 +0900

    fix: WorkationWorkKeyword Entity 및 enum 추가
    - 기존 Workation Entity의 keyword 필드를 Work 키워드와 Vacation 키워드 Entity로 분리하여 일대다 관계로 구성
    - WorkationWorkKeyword Entity 추가

---
+++ src/main/java/com/worknector/offizz/domain/workation/domain/entity/WorkationWorkKeyword.java
@@ -0,0 +1,30 @@
+package com.worknector.offizz.domain.workation.domain.entity;
+
+import com.worknector.offizz.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.EnumType.STRING;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class WorkationWorkKeyword extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long workationWorkKeywordId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workation_id", nullable = false)
+    private Workation workation;
+
+    @Column(nullable = false)
+    @Enumerated(value = STRING)
+    private WorkKeyword keyword;
+}
