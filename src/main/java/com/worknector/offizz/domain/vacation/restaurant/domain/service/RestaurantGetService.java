575359aef0b23f91e7ade8e5c088e03cfe8facd4
jiwon <kiwijomn@gmail.com>
2024-09-10 17:48:49 +0900

    fix: getAllRestaurantBySearch 메서드 반환 타입 변경

--- src/main/java/com/worknector/offizz/domain/vacation/restaurant/domain/service/RestaurantGetService.java
+++ src/main/java/com/worknector/offizz/domain/vacation/restaurant/domain/service/RestaurantGetService.java
@@ -3,18 +3,16 @@ package com.worknector.offizz.domain.vacation.restaurant.domain.service;
 import com.worknector.offizz.domain.vacation.restaurant.domain.entity.Restaurant;
 import com.worknector.offizz.domain.vacation.restaurant.domain.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RestaurantGetService {
     private final RestaurantRepository restaurantRepository;
 
-    public Page<Restaurant> getAllRestaurantBySearch(String search, int page, int size, double lat, double lon) {
-        Pageable pageable = PageRequest.of(page - 1, size);
-        return restaurantRepository.getAllRestaurantBySearch(search, pageable, lat, lon);
+    public List<Restaurant> getAllRestaurantBySearch(String search, double lat, double lon) {
+        return restaurantRepository.getAllRestaurantBySearch(search, lat, lon);
     }
 }
