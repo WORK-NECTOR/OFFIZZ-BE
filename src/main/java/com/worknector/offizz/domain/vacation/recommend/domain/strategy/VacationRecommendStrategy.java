471162034b23da0907ded600c57e72f422c2857c
jiwon <kiwijomn@gmail.com>
2024-09-10 17:12:42 +0900

    feat: VacationRecommend 전략 패턴 적용을 위한 interface

---
+++ src/main/java/com/worknector/offizz/domain/vacation/recommend/domain/strategy/VacationRecommendStrategy.java
@@ -0,0 +1,9 @@
+package com.worknector.offizz.domain.vacation.recommend.domain.strategy;
+
+import com.worknector.offizz.domain.vacation.recommend.application.dto.res.VacationRecommendResponse;
+
+import java.util.List;
+
+public interface VacationRecommendStrategy {
+    List<VacationRecommendResponse> recommend(String search, double lat, double lon);
+}
