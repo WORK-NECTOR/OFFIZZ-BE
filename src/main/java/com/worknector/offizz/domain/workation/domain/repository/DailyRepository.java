e28810a80e447e257d7f97520fdde903581d9c40
yang <ywj9811@gmail.com>
2024-09-20 05:16:16 +0900

    OFZ-30 feat : Repository 생성

---
+++ src/main/java/com/worknector/offizz/domain/workation/domain/repository/DailyRepository.java
@@ -0,0 +1,11 @@
+package com.worknector.offizz.domain.workation.domain.repository;
+
+import com.worknector.offizz.domain.workation.domain.entity.Daily;
+import com.worknector.offizz.domain.workation.domain.entity.Workation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface DailyRepository extends JpaRepository<Daily, Long> {
+    Optional<Daily> findByWorkationAndDay(Workation workation, int day);
+}
