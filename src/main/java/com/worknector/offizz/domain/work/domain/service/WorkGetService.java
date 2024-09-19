ea54f34c632553285fb81e2e19f85cb361c1ae80
yang <ywj9811@gmail.com>
2024-09-20 05:15:46 +0900

    OFZ-30 fix : office,cafe service통합

--- src/main/java/com/worknector/offizz/domain/work/domain/service/WorkGetService.java
+++ src/main/java/com/worknector/offizz/domain/work/domain/service/WorkGetService.java
@@ -1,8 +1,10 @@
 package com.worknector.offizz.domain.work.domain.service;
 
-import com.worknector.offizz.domain.work.presenation.constant.Region;
+import com.worknector.offizz.domain.work.domain.entity.Cafe;
 import com.worknector.offizz.domain.work.domain.entity.Office;
+import com.worknector.offizz.domain.work.domain.repository.CafeRepository;
 import com.worknector.offizz.domain.work.domain.repository.OfficeRepository;
+import com.worknector.offizz.domain.work.presenation.constant.Region;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,11 +13,15 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @Service
-public class OfficeGetService {
+@RequiredArgsConstructor
+public class WorkGetService {
     private final OfficeRepository officeRepository;
+    private final CafeRepository cafeRepository;
 
+    public List<Cafe> allCafeSearchOrLocationPage(String search, double lat, double lon) {
+        return cafeRepository.findAllPagingBySearchOrLocation(search, lat, lon);
+    }
     public List<Office> recommendOffice(Region region, int size) {
         return officeRepository.findRecommendByRegion(region, size);
     }
@@ -26,17 +32,17 @@ public class OfficeGetService {
         return office;
     }
 
-    public Page<Office> allRegionOfficePage(Region region, int page, int size) {
+    public Page<Office> allOfficeRegionOfficePage(Region region, int page, int size) {
         Pageable pageable = PageRequest.of(page-1, size);
         return officeRepository.findAllPagingByRegion(region, pageable);
     }
 
-    public Page<Office> allSearchOfficePage(String search, int page, int size) {
+    public Page<Office> allOfficeSearchOfficePage(String search, int page, int size) {
         Pageable pageable = PageRequest.of(page-1, size);
         return officeRepository.findAllPagingBySearch(search, pageable);
     }
 
-    public List<Office> allSearchOrLocationPage(String search, double lat, double lon) {
+    public List<Office> allOfficeSearchOrLocationPage(String search, double lat, double lon) {
         return officeRepository.findAllPagingBySearchOrLocation(search, lat, lon);
     }
 
