89127bcf174705e235ae40ce31400b4377fcc199
jiwon <kiwijomn@gmail.com>
2024-09-23 19:28:21 +0900

    fix: OnboardingRequest DTO Swagger 형식 수정

--- src/main/java/com/worknector/offizz/domain/workation/application/dto/req/OnboardingRequest.java
+++ src/main/java/com/worknector/offizz/domain/workation/application/dto/req/OnboardingRequest.java
@@ -13,43 +13,43 @@ import java.util.List;
 
 public record OnboardingRequest(
         @NotBlank
-        @Schema(name = "워케이션에 참여하게 된 주된 이유", example = "이유")
+        @Schema(description = "워케이션에 참여하게 된 주된 이유", example = "이유")
         String reason,
         @NotBlank
-        @Schema(name = "어디서 워케이션 참여하는지 (장소명)", example = "씨마크 호텔")
+        @Schema(description = "어디서 워케이션 참여하는지 (장소명)", example = "씨마크 호텔")
         String locate,
         @NotBlank
-        @Schema(name = "어디서 워케이션 참여하는지 (장소 주소)", example = "창해로 307 ...")
+        @Schema(description = "어디서 워케이션 참여하는지 (장소 주소)", example = "창해로 307 ...")
         String address,
         @NotNull
-        @Schema(name = "워케이션 시작 날짜", example = "2024.08.25")
+        @Schema(description = "워케이션 시작 날짜", example = "2024.08.25")
         @JsonFormat(pattern = "yyyy.MM.dd")
         LocalDate startDate,
         @NotNull
-        @Schema(name = "워케이션 종료 날짜", example = "2024.09.05")
+        @Schema(description = "워케이션 종료 날짜", example = "2024.09.05")
         @JsonFormat(pattern = "yyyy.MM.dd")
         LocalDate endDate,
         @NotNull
-        @Schema(name = "코어타임 시작 시각", example = "09:00")
+        @Schema(description = "코어타임 시작 시각", example = "09:00")
         LocalTime startCoreTime,
         @NotNull
-        @Schema(name = "코어타임 종료 시각", example = "12:00")
+        @Schema(description = "코어타임 종료 시각", example = "12:00")
         LocalTime endCoreTime,
         @NotNull
-        @Schema(name = "일 키워드 (OFFICE / CAFE / LIBRARY / STUDYCAFE / STUDYROOM)", example = "[OFFICE]")
+        @Schema(description = "일 키워드 (OFFICE / CAFE / LIBRARY)", example = "[OFFICE]")
         List<WorkKeyword> workKeywords,
         @NotNull
-        @Schema(name = "여행 키워드 (NATURE / RESTAURANT / CULTURE / SHOPPING)", example = "[NATURE]")
+        @Schema(description = "여행 키워드 (NATURE / RESTAURANT / CULTURE / SHOPPING)", example = "[NATURE]")
         List<VacationKeyword> vacationKeywords,
         @NotBlank
-        @Schema(name = "달성하고 싶은 목표", example = "목표")
+        @Schema(description = "달성하고 싶은 목표", example = "목표")
         String goal,
         List<Bucketlist> bucketlists
 ) {
     public record Bucketlist(
-            @Schema(name = "방문 예정인 장소명", example = "강릉 OO해변")
+            @Schema(description = "방문 예정인 장소명", example = "강릉 OO해변")
             String name,
-            @Schema(name = "방문 예정인 장소 위치", example = "창해로 307 ...")
+            @Schema(description = "방문 예정인 장소 위치", example = "창해로 307 ...")
             String address
     ) {}
 }
