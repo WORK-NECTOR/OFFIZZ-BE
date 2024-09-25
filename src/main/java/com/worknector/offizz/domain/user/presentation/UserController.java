8a5b52563c6a57d4242672b0d452554a2512c253
jiwon <kiwijomn@gmail.com>
2024-09-25 16:01:29 +0900

    OFZ-38 feat: 개인정보 수정 API 구현

--- src/main/java/com/worknector/offizz/domain/user/presentation/UserController.java
+++ src/main/java/com/worknector/offizz/domain/user/presentation/UserController.java
@@ -1,5 +1,6 @@
 package com.worknector.offizz.domain.user.presentation;
 
+import com.worknector.offizz.domain.user.application.dto.req.UserInfoRequest;
 import com.worknector.offizz.domain.user.application.dto.res.UserInfoResponse;
 import com.worknector.offizz.domain.user.application.usecase.UserUseCase;
 import com.worknector.offizz.domain.user.domain.entity.User;
@@ -8,9 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +23,14 @@ public class UserController {
     @Operation(summary = "마이페이지 > 개인정보 조회", description = "토큰으로 요청 -> 개인정보 반환")
     public ResponseEntity<UserInfoResponse> getUserInfo(@AuthenticationPrincipal User user) {
         UserInfoResponse userInfo = userUseCase.getUserInfo(user);
+        return ResponseEntity.ok(userInfo);
+    }
+
+    @PatchMapping
+    @Operation(summary = "마이페이지 > 개인정보 수정", description = "토큰으로 요청 -> 개인정보 수정")
+    public ResponseEntity<UserInfoResponse> updateUserInfo(@AuthenticationPrincipal User user,
+                                                           @RequestBody UserInfoRequest userInfoRequest) {
+        UserInfoResponse userInfo = userUseCase.updateUserInfo(user, userInfoRequest);
         return ResponseEntity.ok(userInfo);
     }
 }
