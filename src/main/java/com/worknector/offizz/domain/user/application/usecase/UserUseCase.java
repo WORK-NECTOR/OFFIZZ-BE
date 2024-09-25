0c4265bb81f056adb93335324447b928862fb762
jiwon <kiwijomn@gmail.com>
2024-09-25 16:01:06 +0900

    OFZ-38 feat: 개인정보 수정 usecase method 구현

--- src/main/java/com/worknector/offizz/domain/user/application/usecase/UserUseCase.java
+++ src/main/java/com/worknector/offizz/domain/user/application/usecase/UserUseCase.java
@@ -1,5 +1,6 @@
 package com.worknector.offizz.domain.user.application.usecase;
 
+import com.worknector.offizz.domain.user.application.dto.req.UserInfoRequest;
 import com.worknector.offizz.domain.user.application.dto.res.UserInfoResponse;
 import com.worknector.offizz.domain.user.domain.entity.User;
 import com.worknector.offizz.domain.user.domain.service.UserGetService;
@@ -18,6 +19,12 @@ public class UserUseCase {
 
     public UserInfoResponse getUserInfo(User user) {
         User findUser = userGetService.byUserId(user.getUserId());
+        return mapToUserInfoResponse(findUser);
+    }
+
+    public UserInfoResponse updateUserInfo(User user, UserInfoRequest userInfoRequest) {
+        User findUser = userGetService.byUserId(user.getUserId());
+        findUser.updateNickName(userInfoRequest.nickName());
         return mapToUserInfoResponse(findUser);
     }
 }
