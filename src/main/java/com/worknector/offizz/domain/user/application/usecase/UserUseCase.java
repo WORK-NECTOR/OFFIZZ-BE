e4abab24dcc4597a984182707dd59680bc208ac4
jiwon <kiwijomn@gmail.com>
2024-09-25 16:19:09 +0900

    OFZ-38 feat: 개인정보 조회 및 수정 method 정상 회원 확인 validation 추가

--- src/main/java/com/worknector/offizz/domain/user/application/usecase/UserUseCase.java
+++ src/main/java/com/worknector/offizz/domain/user/application/usecase/UserUseCase.java
@@ -4,11 +4,13 @@ import com.worknector.offizz.domain.user.application.dto.req.UserInfoRequest;
 import com.worknector.offizz.domain.user.application.dto.res.UserInfoResponse;
 import com.worknector.offizz.domain.user.domain.entity.User;
 import com.worknector.offizz.domain.user.domain.service.UserGetService;
+import com.worknector.offizz.global.exception.ExceptionResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static com.worknector.offizz.domain.user.application.mapper.UserInfoMapper.mapToUserInfoResponse;
+import static com.worknector.offizz.domain.user.presentation.constant.UserResponseCode.USER_DELETED;
 
 @RequiredArgsConstructor
 @Service
@@ -19,11 +21,13 @@ public class UserUseCase {
 
     public UserInfoResponse getUserInfo(User user) {
         User findUser = userGetService.byUserId(user.getUserId());
+        validateUsableUser(findUser);
         return mapToUserInfoResponse(findUser);
     }
 
     public UserInfoResponse updateUserInfo(User user, UserInfoRequest userInfoRequest) {
         User findUser = userGetService.byUserId(user.getUserId());
+        validateUsableUser(findUser);
         findUser.updateNickName(userInfoRequest.nickName());
         return mapToUserInfoResponse(findUser);
     }
@@ -31,5 +35,11 @@ public class UserUseCase {
     public Long withdrawUser(User user) {
         user.changeStatusToDeleted();
         return user.getUserId();
+    }
+
+    private void validateUsableUser(User user) {
+        if (user.isDeleted()) {
+            new ExceptionResponse(USER_DELETED.getCode(), USER_DELETED.getMessage());
+        }
     }
 }
