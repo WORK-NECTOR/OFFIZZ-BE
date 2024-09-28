388788c7471aaf087ca13bf63059fc1ee75e8360
yang <ywj9811@gmail.com>
2024-09-29 06:32:53 +0900

    OFZ-42 feat : 설명 추가

--- src/main/java/com/worknector/offizz/domain/workation/presentation/EntireRetrospectController.java
+++ src/main/java/com/worknector/offizz/domain/workation/presentation/EntireRetrospectController.java
@@ -4,6 +4,7 @@ import com.worknector.offizz.domain.user.domain.entity.User;
 import com.worknector.offizz.domain.workation.application.dto.req.EntireRetrospectRequest;
 import com.worknector.offizz.domain.workation.application.dto.res.RecapResponse;
 import com.worknector.offizz.domain.workation.application.usecase.EntireRetrospectUseCase;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -19,6 +20,7 @@ public class EntireRetrospectController {
     private final EntireRetrospectUseCase retrospectUseCase;
 
     @PostMapping("/recap")
+    @Operation(summary = "워케이션 완료 및 리캡 생성", description = "토큰과 전체 회고 (평가 및 내용) 전달해주시면 됩니다")
     public ResponseEntity<RecapResponse> workationFinish(@AuthenticationPrincipal User user, @RequestBody EntireRetrospectRequest request) {
         RecapResponse recapResponse = retrospectUseCase.saveRetrospect(user, request);
         return ResponseEntity.ok(recapResponse);
