e2bd5444ee38e4cfeebcfd694db1af8140ee6cec
yang <ywj9811@gmail.com>
2024-09-29 08:33:05 +0900

    OFZ-42 feat : 대시보드 빠진 기능 추가

--- src/main/java/com/worknector/offizz/domain/workation/domain/entity/VacationTodo.java
+++ src/main/java/com/worknector/offizz/domain/workation/domain/entity/VacationTodo.java
@@ -1,6 +1,7 @@
 package com.worknector.offizz.domain.workation.domain.entity;
 
 import com.worknector.offizz.domain.workation.application.dto.req.VacationTodoFinRequest;
+import com.worknector.offizz.domain.workation.application.dto.req.VacationTodoRequest;
 import com.worknector.offizz.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -51,5 +52,24 @@ public class VacationTodo extends BaseEntity {
         this.isComplete = true;
         if (request.image() != null)
             this.image = request.image();
+    }
+
+    public void updateVacationTodo(VacationTodoFinRequest request) {
+        this.locate = request.locate();
+        this.rating = request.rating();
+        this.comment = request.comment();
+        this.isComplete = true;
+        if (request.image() != null)
+            this.image = request.image();
+        else
+            this.image = null;
+    }
+
+    public void updateNotFin() {
+        this.locate = null;
+        this.rating = null;
+        this.comment = null;
+        this.isComplete = false;
+        this.image = null;
     }
 }
