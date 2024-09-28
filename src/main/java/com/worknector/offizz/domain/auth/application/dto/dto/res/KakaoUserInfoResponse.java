993d77abcf4689fb23a525c7487293be58a07313
yang <ywj9811@gmail.com>
2024-09-29 07:32:13 +0900

    OFZ-42 fix : 카카오 로그인 이메일 오류 수정

--- src/main/java/com/worknector/offizz/domain/auth/application/dto/dto/res/KakaoUserInfoResponse.java
+++ src/main/java/com/worknector/offizz/domain/auth/application/dto/dto/res/KakaoUserInfoResponse.java
@@ -1,5 +1,5 @@
 package com.worknector.offizz.domain.auth.application.dto.dto.res;
 
-public record KakaoUserInfoResponse(Long id, KakaoAccount kakaoAccount) {
+public record KakaoUserInfoResponse(Long id, KakaoAccount kakao_account) {
     public record KakaoAccount(String email) {}
 }
