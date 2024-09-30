da09b9d04e7ff5826871054febd9d4e59d9aaf03
jiwon <kiwijomn@gmail.com>
2024-09-30 15:40:54 +0900

    OFZ-44 fix: vacation 추천 API AuthenticationPrincipal 어노테이션 추가

--- src/main/java/com/worknector/offizz/domain/vacation/recommend/presentation/VacationRecommendController.java
+++ src/main/java/com/worknector/offizz/domain/vacation/recommend/presentation/VacationRecommendController.java
@@ -1,5 +1,6 @@
 package com.worknector.offizz.domain.vacation.recommend.presentation;
 
+import com.worknector.offizz.domain.user.domain.entity.User;
 import com.worknector.offizz.domain.vacation.recommend.application.dto.res.PagingVacationRecommendResponse;
 import com.worknector.offizz.domain.vacation.recommend.application.usecase.VacationRecommendUseCase;
 import com.worknector.offizz.domain.vacation.recommend.presentation.constant.Filter;
@@ -8,20 +9,22 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/vacation/recommend")
+@RequestMapping("/api/vacation")
 @Tag(name = "Vacation Recommend Controller")
 public class VacationRecommendController {
 
     private final VacationRecommendUseCase vacationRecommendUseCase;
 
-    @GetMapping("/{filter}/{page}/{size}")
+    @GetMapping("/recommend/{filter}/{page}/{size}")
     @Operation(summary = "VACATION > 추천) 검색에 따른 vacation 페이지",
             description = "'서울 강남' 검색 -> 서울과 강남을 모두 포함하는 주소 혹은 자연/맛집/문화예술/쇼핑 이름")
     public ResponseEntity<PagingVacationRecommendResponse> searchRecommendVacation(
+            @AuthenticationPrincipal User user,
             @Schema(description = "vacation 필터: culture, nature, restaurant, shopping, all")
             @PathVariable Filter filter,
             @RequestParam(name = "search") String search,
@@ -29,7 +32,7 @@ public class VacationRecommendController {
             @RequestParam(name = "lon") double lon,
             @PathVariable(name = "page") int page,
             @PathVariable(name = "size") int size) {
-        PagingVacationRecommendResponse recommendVacations = vacationRecommendUseCase.getRecommendVacation(filter, search, lat, lon, page, size);
+        PagingVacationRecommendResponse recommendVacations = vacationRecommendUseCase.getRecommendVacation(filter, search, lat, lon, user, page, size);
         return ResponseEntity.ok(recommendVacations);
     }
 }
