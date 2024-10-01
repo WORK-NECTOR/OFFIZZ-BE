d633be932a2556402eab9688c184504e1f967f4e
yang <ywj9811@gmail.com>
2024-10-02 07:47:11 +0900

    OFZ-42 feat : 전체 리캡 조회 및 상세조회 추가

--- src/main/java/com/worknector/offizz/domain/workation/application/mapper/WorkationMapper.java
+++ src/main/java/com/worknector/offizz/domain/workation/application/mapper/WorkationMapper.java
@@ -2,6 +2,7 @@ package com.worknector.offizz.domain.workation.application.mapper;
 
 import com.worknector.offizz.domain.user.domain.entity.User;
 import com.worknector.offizz.domain.workation.application.dto.req.OnboardingRequest;
+import com.worknector.offizz.domain.workation.application.dto.res.AllRecapResponse;
 import com.worknector.offizz.domain.workation.domain.entity.Workation;
 
 public class WorkationMapper {
@@ -22,5 +23,9 @@ public class WorkationMapper {
                 .endCoreTime(onboardingRequest.endCoreTime())
                 .goal(onboardingRequest.goal())
                 .build();
+    }
+
+    public static AllRecapResponse mapToRecapResponse(Workation workation) {
+        return new AllRecapResponse(workation.getWorkationId(), workation.getLocate(), workation.getAddress(), workation.getStartDate(), workation.getEndDate());
     }
 }
