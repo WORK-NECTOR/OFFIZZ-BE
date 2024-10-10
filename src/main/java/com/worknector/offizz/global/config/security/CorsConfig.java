373dedf241156d574cd79a52f29a5219d2837d31
yang <ywj9811@gmail.com>
2024-10-11 01:33:48 +0900

    HotFix : cors설정 수정

--- src/main/java/com/worknector/offizz/global/config/security/CorsConfig.java
+++ src/main/java/com/worknector/offizz/global/config/security/CorsConfig.java
@@ -11,8 +11,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedOrigins("*")
+                .allowedMethods("*")
                 .allowCredentials(true)
                 .exposedHeaders(HttpHeaders.LOCATION);
         WebMvcConfigurer.super.addCorsMappings(registry);
