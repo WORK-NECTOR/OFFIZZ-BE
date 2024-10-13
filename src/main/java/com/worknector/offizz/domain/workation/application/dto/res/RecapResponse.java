95d1a9776515b697d1aa0a31e42358950f611c79
yang <ywj9811@gmail.com>
2024-10-13 10:49:14 +0900

    HotFix : 리캡 상세조회 수정

--- src/main/java/com/worknector/offizz/domain/workation/application/dto/res/RecapResponse.java
+++ src/main/java/com/worknector/offizz/domain/workation/application/dto/res/RecapResponse.java
@@ -11,16 +11,14 @@ public record RecapResponse(
     FourthPage fourthPage,
     FifthPage fifthPage,
     SixthPage sixthPage,
-    SeventhPage seventhPage,
-    EighthPage eighthPage
+    SeventhPage seventhPage
 ) {
     public record FirstPage(String reason){}
     public record SecondPage(long totalTime, LocalTime startTime, LocalTime endTime, long averageTime){}
     public record ThirdPage(double rate){}
     public record FourthPage(LocalDate passionDate, long diff){}
-    public record FifthPage(List<LocalDate> bestWorkDays){}
-    public record SixthPage(int totalVacation, double averageRate){}
-    public record SeventhPage(List<LocalDate> bestVacationDays){}
-    public record EighthPage(List<BestVacation> bestVacations){}
+    public record FifthPage(int totalVacation, double averageRate){}
+    public record SixthPage(double rate){}
+    public record SeventhPage(List<BestVacation> bestVacations){}
     public record BestVacation(LocalDate date, String name){}
 }
